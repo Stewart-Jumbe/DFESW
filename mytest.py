16d0df12a2a8c351736b91aacce52f042a0bc180
Stewart <stewartjumbe@gmail.com>
2021-10-01 11:59:46 +0100

    Added some python code

--- mytest.py
+++ mytest.py
@@ -1,1 +1,14 @@
+variableNameForAString ='This is a String'
+variableForAFloat = 15.0
+variableForAnInteger = 121
+variableForABoolean = True
+
+
 print('I can python')
+a = ('    *\n  *  *\n *  *  *\n*       * \n')
+print(a)
+
+#number1 = float(input("Please enter the first number: "))
+#number2 = float(input("Please enter the second number: "))
+#answer = number1 + number2
+#print(number1, "+", number2, "=", answer)
---
+++ polite.py
@@ -0,0 +1,4 @@
+####DFESW3 QA community exercises
+politeFirstName = input("Please enter your first name: ")
+politeLastName = input("Please enter your last name: ")
+hello= print(f'Hello {politeFirstName} {politeLastName}') # using f-string literals
