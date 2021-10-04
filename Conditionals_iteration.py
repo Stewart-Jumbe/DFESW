bcf11c8ef5a785a68cc38d8dffcba4a114ca2b31
Stewart <stewartjumbe@gmail.com>
2021-10-04 10:44:54 +0100

    Completed tutoria and excercises

---
+++ Conditionals_iteration.py
@@ -0,0 +1,46 @@
+##tutorial
+#devs_money = 100
+#dev_can_play_smash = False
+
+#if devs_money > 10 and dev_can_play_smash:
+#    print("Dev enters a smash tournament!")
+#elif devs_money < 10 and dev_can_play_smash:
+ #   print ("Dev us too poor to enter")
+#else:
+#    print("Dev just can't play smash")
+
+
+
+####Exercises
+#mark = input("Please enter your mark: ")
+
+#if int(mark) > 95:
+#    print("Distinction")
+#elif 65<= int(mark) <= 85 :
+#    print("Pass")
+#print("Fail") # if not within either range
+
+###ITERATION
+
+#Tutorial
+#count = 0
+#name = str(input("What is your name?"))
+
+#while count < 5:
+#    print(f'{name} is awesome!')
+#    count += 1
+
+#for  i in range (5,11):
+#    print (i)
+
+
+####Exercise
+
+
+
+count = 0
+
+while count <= 4:
+    name1 = input("please enter name: ")
+    print(f'{name1} is Awesome')
+    count +=1
