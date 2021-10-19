ea54db2021941e8c89b7c6d3a73a74bd430e4f68
Stewart <stewartjumbe@gmail.com>
2021-10-19 20:55:54 +0100

    updated file

--- file_stuff.py
+++ file_stuff.py
@@ -13,16 +13,54 @@
 # #openedfile.write()
 # openedfile.close()
 
-file = open("filename.txt", "r")
+# file = open("filename.txt", "r")
 
-outfile = ""
+# outfile = ""
 
-for line in range(1, 10):
-    if line % 2 == 0:
-        outfile += file.readline()
-    else:
-        file.readline()
+# for line in range(1, 10):
+#     if line % 2 == 0:
+#         outfile += file.readline()
+#     else:
+#         file.readline()
 
-file = open("filename.txt", "w")
-file.write(outfile)
-file.close()
+# file = open("filename.txt", "w")
+# file.write(outfile)
+# file.close()
+
+####QA exe on files
+#Create a Python file which does the following:
+
+#Opens a new text file called "teams.txt" and adds the names of 5 sports teams.
+# Team =["Crouch Potato","Moves like Agger","Clippedy Klopp","Pleased to Michu","No Kane, No Gain"]
+
+# with open('teams.txt','w') as myfile: #opening file in write mode and assinging the open file an alias
+    
+#     for i in range(len(Team)):
+#         myfile.writelines(Team[i])
+#         myfile.writelines('\n')       
+# # myfile.close()
+
+#Reads and displays the names of the 1st and 4th team in the file.
+# with open('teams.txt','r') as myfile: #Opening file to read contents
+#     lines = myfile.readlines()
+
+#     print(lines [1])
+#     print(lines [4])
+
+# myfile.close()
+
+
+# Create a new Python file which does the following:
+# Edits your "teams.txt" file so that the top line is replaced with "This is a new line".
+# Print out the edited file line by line.
+
+with open('teams.txt') as myfile:
+
+    lines =myfile.readlines()
+
+    #writing a new line
+    lines.insert(0, "5 Aside football teams \n") # online 
+
+with open('teams.txt','w') as myfile:
+    myfile.writelines(lines)
+
