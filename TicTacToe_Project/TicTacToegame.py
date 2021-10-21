d2511d8a4c3138a71c3a6834be9ecc965f834d32
Stewart <stewartjumbe@gmail.com>
2021-10-21 22:16:39 +0100

    Created Java files covering various topics

--- TicTacToe_Project/TicTacToegame.py
+++ TicTacToe_Project/TicTacToegame.py
@@ -10,27 +10,30 @@ def clear():
 
 
 #assigining values of board    
-board = ['#','1','2','3','4','5','6','7','8','9'] # used to display the noughts and crosses board + update
+board = ['#','','','','','','','','',''] # used to display the noughts and crosses board + update
 
-#code to display the board
+#1.0 code to display the board
 def displayBoard(board):
     #enumerated_board =enumerate(board)
     
 
     
     #print(list(enumerated_board))
+    print("\n")
     print(f"{board[1]} | {board[2]} | {board[3]}")
     print(f"_________\n")
     print(f"{board[4]} | {board[5]} | {board[6]}")
     print(f"_________\n")
     print(f"{board[7]} | {board[8]} | {board[9]}")
+    print("\n")
 
 ## Testing the code above ##
 testBoard = ['#','X','O','X','O','X','O','X','O','X']
 displayBoard(testBoard)
 
-##functio that takes in a player input and assigns their marker as X or O
+#2.0 functio that takes in a player input and assigns their marker as X or O
 def playerInput():
+    
     
     #acceptable values
     validInput = ['X','O']
@@ -56,11 +59,13 @@ def playerInput():
 ## Testing player input 
 playerInput()
 
-#Function that takes in the board list object, a player symbol (X or O), and a desired position (number 1-9)
+#3.0Function that takes in the board list object, a player symbol (X or O), and a desired position (number 1-9)
 def markerPos():
     
     
-    displayBoard(board)
+    #displaying numbered board to help user choose grid position
+    numberedBoard= ['#',1,2,3,4,5,6,7,8,9]
+    displayBoard(numberedBoard)
     
     #acceptable values
     validInput = [num for num in range(1,10)]
@@ -84,7 +89,7 @@ def markerPos():
 ##Testing MarkerPos function
 markerPos()
 
-# Function for placing the players chosen symbol on the board
+# 4.0 Function for placing the players chosen symbol on the board
 def placeMarker(board, playerSym, gridPos):
     
     board[gridPos]= playerSym # placing the players symbol on the chosen grid position
@@ -95,17 +100,16 @@ def placeMarker(board, playerSym, gridPos):
 
 placeMarker(board,playerSym,gridPos)
 
-#Function to check whether the placement of a player symbol has won the game
+#4.5 Function to check whether the placement of a player symbol has won the game
 
 def win_check(board, mark):
     #winning lists
-    
+    status = "none" # initialising status
     if board[1:4].count(mark)==3 or board[4:7].count(mark)==3 or board[7:10].count(mark)==3:
         status = "You've Won"
     #diagonals
     elif board[1]=='X' and board[5] =='X' and board[9]=='X' or  board[3]=='X' and board[5]=='X' and board[7]=='X':
         status = "You've Won"
-    
     else: pass
 
     return status
@@ -113,5 +117,30 @@ def win_check(board, mark):
 win_check(board,'O')
 
 
+#5.Function that uses random module to choose which player goes first
 
+def choosePlayer():
+    from random import randint
+    num = randint(1,2)
+    return f"Player {num} will go first"
+
+#Testing choosePlayer function
+print(choosePlayer())
+
+#6. function to check if grid position is empty or filled
+def spaceCheck(board,gridPos):
+
+    if board[gridPos]!= "":
+        status= "Position already filled"
+    return status
+
+##run test to check if the above works
+
+#7. Function to check whether theirs empty space on the board
+board=['#','O','X','','X','O','O','O','O','O']
+
+def fullBoardCheck():
+    #if board is full there should be no empty spaces
+    return board.count("")== 0
+print(fullBoardCheck())
 
